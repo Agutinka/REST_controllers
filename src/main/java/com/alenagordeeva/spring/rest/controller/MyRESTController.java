20356f147548ff1e953ccb0f7248d9254498de47
Agutinka <alenaagutina24@gmail.com>
2024-01-31 10:47:00 +0300

    добавлено удаление работника по id

--- src/main/java/com/alenagordeeva/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/alenagordeeva/spring/rest/controller/MyRESTController.java
@@ -7,6 +7,7 @@ import com.alenagordeeva.spring.rest.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,5 +57,16 @@ public class MyRESTController {
     public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
         return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee (@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
     }
 }
