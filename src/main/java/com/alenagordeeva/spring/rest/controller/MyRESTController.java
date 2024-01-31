2b69ccabb857a02b0e99ce619157dcb9ec8b50c2
Agutinka <alenaagutina24@gmail.com>
2024-01-31 10:50:15 +0300

    Убрала лишнее

--- src/main/java/com/alenagordeeva/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/alenagordeeva/spring/rest/controller/MyRESTController.java
@@ -1,14 +1,10 @@
 package com.alenagordeeva.spring.rest.controller;
 
-import com.alenagordeeva.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.alenagordeeva.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.alenagordeeva.spring.rest.service.EmployeeService;
 import com.alenagordeeva.spring.rest.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
